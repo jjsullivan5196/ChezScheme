4b87fe29b7c918cbf05e8612cc1f995b97422bbf
John Sullivan <john-j-sullivan@outlook.com>
2024-05-29 20:09:58 -0700

    Build APE with github action

---
+++ .github/cosmo/build-ape.sh
@@ -0,0 +1,43 @@
+# Chez build settings
+SCHEME_BIN="scheme.com"
+PREFIX=/zip/usr
+CONFIG_FLAGS="--prefix=$PREFIX --installabsolute --installschemename=$SCHEME_BIN --disable-x11 --disable-curses"
+
+# cosmocc toolchain
+COSMOCC=/opt/cosmocc
+export PATH="$COSMOCC/bin:$PATH"
+
+# need to disable zipos so we can use /zip as part of the install
+# prefix
+export COSMOPOLITAN_DISABLE_ZIPOS=1
+
+# prefix and build dirs
+sudo mkdir -p $PREFIX
+sudo chown -R "$(id -u):$(id -g)" $PREFIX
+mkdir ./dist
+
+# build host amd64 Chez
+export CC=$COSMOCC/bin/x86_64-unknown-cosmo-cc
+export AR=$COSMOCC/bin/x86_64-unknown-cosmo-ar
+./configure ${CONFIG_FLAGS}
+make -j
+make install
+cp $PREFIX/bin/$SCHEME_BIN ./dist/$SCHEME_BIN.ta6le
+
+# cross compile aarch64
+export CC=$COSMOCC/bin/aarch64-unknown-cosmo-cc
+export AR=$COSMOCC/bin/aarch64-unknown-cosmo-ar
+./configure -m=tarm64le ${CONFIG_FLAGS}
+make -j
+make install
+cp $PREFIX/bin/$SCHEME_BIN ./dist/$SCHEME_BIN.tarm64le
+
+# copy results and dump unnecessary files
+cp -r $PREFIX ./dist/
+rm -rf ./dist/usr/bin ./dist/usr/share
+cd dist
+
+# produce the APE image
+apelink -o $SCHEME_BIN.zip $SCHEME_BIN.ta6le $SCHEME_BIN.tarm64le
+zip -q -r $SCHEME_BIN.zip usr/
+mv ./$SCHEME_BIN.zip ./$SCHEME_BIN
---
+++ .github/cosmo/setup.sh
@@ -0,0 +1,17 @@
+COSMOCC_VERSION=3.3.10
+COSMOCC_ZIP=/opt/cosmocc/cosmocc-$COSMOCC_VERSION.zip
+COSMOCC_SHA256="00d61c1215667314f66e288c8285bae38cc6137fca083e5bba6c74e3a52439de"
+
+function fail_sha
+{
+    >&2 echo "Downloaded file ${1} SHA256 does not match known hash."
+    exit 1
+}
+
+sudo mkdir -p /opt/cosmocc
+sudo chown -R "$(id -u):$(id -g)" /opt/cosmocc
+
+wget --quiet -O $COSMOCC_ZIP "https://cosmo.zip/pub/cosmocc/cosmocc-${COSMOCC_VERSION}.zip"
+(echo "${COSMOCC_SHA256} ${COSMOCC_ZIP}" | sha256sum --check --status) || fail_sha $COSMOCC_ZIP
+
+unzip -q $COSMOCC_ZIP -d /opt/cosmocc/
